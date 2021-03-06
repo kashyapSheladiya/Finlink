b86e027873040f849b9b5669491554fa3b8fa56d
kashyapSheladiya <kashyap27041993@gmail.com>
2021-03-06 18:59:05 +0100

    migration file update

--- db/migrate/20210306144820_create_api_v1_readings.rb
+++ db/migrate/20210306144820_create_api_v1_readings.rb
@@ -1,7 +1,7 @@
 class CreateApiV1Readings < ActiveRecord::Migration[5.2]
   def change
     create_table :api_v1_readings do |t|
-      t.references :thermostat, foreign_key: true
+      t.references :thermostat, foreign_key: false
       t.integer :number
       t.decimal :temperature
       t.decimal :humidity
@@ -9,5 +9,6 @@ class CreateApiV1Readings < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
+    add_foreign_key :api_v1_readings, :api_v1_thermostats, column: :thermostat_id
   end
 end
