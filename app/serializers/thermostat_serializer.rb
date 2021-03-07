0f7ef1bdee45c3c560c914f7385658edec398ba2
kashyapSheladiya <kashyap27041993@gmail.com>
2021-03-07 01:19:37 +0100

    customizing serialized HTTP response

--- app/serializers/thermostat_serializer.rb
+++ app/serializers/thermostat_serializer.rb
@@ -1,42 +1,39 @@
 class ThermostatSerializer < ActiveModel::Serializer
-  attributes :id, :household_token, :location,
-    :average_temperature, :minimum_temperature, :maximum_temperature,
-    :average_humidity, :minimum_humidity, :maximum_humidity,
-    :average_battery_charge, :minimum_battery_charge, :maximum_battery_charge
+  attributes :id, :household_token, :location, :temperature, :humidity, :battery_charge
 
-  def average_temperature
-    object.readings.count == 0 ? 0 : object.readings.average(:temperature).round(2)
-  end
+    def temperature
+      hash = {:average => 0, :minimum => 0, :maximum => 0}
+      record = object.readings
+      unless record.count == 0
+        average = object.readings.average(:temperature).round(2)
+        minimum = object.readings.minimum(:temperature).round(2)
+        maximum = object.readings.maximum(:temperature).round(2)
+        hash = {:average => average, :minimum => minimum, :maximum => maximum}
+      end
+      hash
+    end
 
-  def minimum_temperature
-    object.readings.count == 0 ? 0 : object.readings.minimum(:temperature).round(2)
-  end
+    def humidity
+      hash = {:average => 0, :minimum => 0, :maximum => 0}
+      record = object.readings
+      unless record.count == 0
+        average = object.readings.average(:humidity).round(2)
+        minimum = object.readings.minimum(:humidity).round(2)
+        maximum = object.readings.maximum(:humidity).round(2)
+        hash = {:average => average, :minimum => minimum, :maximum => maximum}
+      end
+      hash
+    end
 
-  def maximum_temperature
-    object.readings.count == 0 ? 0 : object.readings.maximum(:temperature).round(2)
-  end
-
-  def average_humidity
-    object.readings.count == 0 ? 0 : object.readings.average(:humidity).round(2)
-  end
-
-  def minimum_humidity
-    object.readings.count == 0 ? 0 : object.readings.minimum(:humidity).round(2)
-  end
-
-  def maximum_humidity
-    object.readings.count == 0 ? 0 : object.readings.maximum(:humidity).round(2)
-  end
-
-  def average_battery_charge
-    object.readings.count == 0 ? 0 : object.readings.average(:battery_charge).round(2)
-  end
-
-  def minimum_battery_charge
-    object.readings.count == 0 ? 0 : object.readings.minimum(:battery_charge).round(2)
-  end
-
-  def maximum_battery_charge
-    object.readings.count == 0 ? 0 : object.readings.maximum(:battery_charge).round(2)
-  end
+    def battery_charge
+      hash = {:average => 0, :minimum => 0, :maximum => 0}
+      record = object.readings
+      unless record.count == 0
+        average = object.readings.average(:battery_charge).round(2)
+        minimum = object.readings.minimum(:battery_charge).round(2)
+        maximum = object.readings.maximum(:battery_charge).round(2)
+        hash = {:average => average, :minimum => minimum, :maximum => maximum}
+      end
+      hash
+    end
 end
